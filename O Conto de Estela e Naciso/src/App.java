8ad5d38060b28958fe552e1e562b33a45948b926
Michelly Nicoly <michelly.nicoly@ufrpe.br>
2022-12-04 20:27:00 -0300

    capítulo b com escolhas incluído

--- O Conto de Estela e Naciso/src/App.java
+++ O Conto de Estela e Naciso/src/App.java
@@ -20,19 +20,41 @@ public class App {
         String escolha1 = escaneador.nextLine(); //capturar o que o usuário escolheu/escrevu
 
         if (escolha1.equals(escolhaA1)){ //comparando a escolha
-        System.out.println("\nApesar de querer ir além\n" +
-        "Recordo como da paixão é ser refém\n" +
-        "E da promessa de me entregar a ninguém\n" +
-        "Resolvi fazer jus e esquecer de um provável porém\n\n");    
+            System.out.println("\nApesar de querer ir além\n" +
+            "Recordo como da paixão é ser refém\n" +
+            "E da promessa de me entregar a ninguém\n" +
+            "Resolvi fazer jus e esquecer de um provável porém\n\n");    
         }
         else if (escolha1.equals(escolhaA2)){ //.equals é uma maneira de comparar string já que esse tipo não é primitivo não podendo assim usar ==
-        System.out.println("\nEla era bela\n" +
-        "Tão bela quanto a energia dela\n" +
-        "Mesmo à distância sentia sua confiança\n" + 
-        "E o sorriso, ah…\n" +
-        "Por ele me faria o prisioneiro se ela fosse a Lisbela\n\n");    
-        }
+            String textoCapituloB =("\nEla era bela\n" +
+            "Tão bela quanto a energia dela\n" +
+            "Mesmo à distância sentia sua confiança\n" + 
+            "E o sorriso, ah…\n" +
+            "Por ele me faria o prisioneiro se ela fosse a Lisbela\n\n");
+            String escolhaB1 = "a vida não e uma novela";
+            String escolhaB2 = "quem e ela?";
 
+            System.out.println(textoCapituloB);
+            System.out.println("-" + escolhaB1);
+            System.out.println("-" + escolhaB2);
+
+            escaneador = new Scanner(System.in); //capturar a entrada do usuário com a variável do tipo Scanner que se chama escaneador
+            String escolha2 = escaneador.nextLine(); 
+
+            if (escolha2.equals(escolhaB1)){
+                System.out.println("\nEssa reflexão me lembrou Caetano\n" +
+                "E ele me lembrou que me encontrar ainda estou tentando\n" +
+                "Então passado alguns segundo me peguei cogitando\n" +
+                "Talvez a melhor opção seja seguir SOZINHO o meu plano\n\n");
+            }
+            else if (escolha2.equals(escolhaB2)){
+                System.out.println("\nE como diria Baco\n" + 
+                "\"Quem é ela? Quem é ela?\"\n" +
+                "Reflexivo acendi um cigarro\n" +
+                "Até que ouvi gritarem um nome\n" +
+                "Estela\n\n");
+            }
         escaneador.close();
+        }
     }
 }
