e89aa047e463bb03951b8af9fb94114679926a11
Michelly Nicoly <michelly.nicoly@ufrpe.br>
2022-12-22 13:52:48 -0300

    método escolher finalizado com loop

--- O Conto de Estela e Naciso/src/O Conto/src/App.java
+++ O Conto de Estela e Naciso/src/O Conto/src/App.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner escaneador = new Scanner (System.in);
+        Scanner escaneador = new Scanner (System.in);  //capturar a entrada do usuário com a variável do tipo Scanner que se chama escaneador
 
         Personagem Narciso = new Personagem ("Narciso",100);
         Personagem Estela = new Personagem ("Estela", 100);
 
         capitulo capA = new capitulo(
             "O POSSÍVEL PORÉM",
+            Narciso,
+            0,
             "Naquela cidade pequena\n" + 
             "Sentia que pra mim não havia alguém\n" +
             "Até que um dia, reparei numa cena\n" +
             "E pensei em talvez existir um porém\n", 
-            Narciso,
-            0,
             "\nO que pensar?\n", 
             "nao existe um porem", 
             "prosseguir e ir alem",
@@ -22,12 +22,12 @@ public class App {
 
         capitulo capB1 = new capitulo(
             "TRAUMAS", 
+            Narciso,
+            -100,
             "Apesar de querer ir além\n" +
             "Recordo como da paixão é ser refém\n" +
             "E da promessa de me entregar a ninguém\n" +
             "Resolvi fazer jus e esquecer de um provável porém\n", 
-            Narciso,
-            -100,
             null,
             null,
             null, //passando o parâmetro como nulo
@@ -35,13 +35,13 @@ public class App {
 
         capitulo capB2 = new capitulo(
             "LISBELA E O PRISIONEIRO", 
+            Narciso,
+            15,
             "Ela era bela\n" +
             "Tão bela quanto a energia dela\n" +
             "Mesmo à distância sentia sua confiança\n" + 
             "E o sorriso, ah…\\n" +
             "Por ele me faria o prisioneiro se ela fosse a Lisbela\n", 
-            Narciso,
-            15,
             "\nO que pensar?\n", 
             "a vida não e uma novela", 
             "quem e ela?",
@@ -49,12 +49,12 @@ public class App {
          
         capitulo capC1 = new capitulo(
             "CAETANO ME AVISOU", 
+            Narciso,
+            -100, 
             "Essa reflexão me lembrou Caetano\n" +
             "E ele me lembrou que me encontrar ainda estou tentando\n" +
             "Então passado alguns segundo me peguei cogitando\n" +
             "Talvez a melhor opção seja seguir SOZINHO o meu plano\n", 
-            Narciso,
-            -100, 
             null, 
             null,
             null,
@@ -62,13 +62,13 @@ public class App {
 
         capitulo capC2 = new capitulo(
             "QUEM É ELA", 
+            Narciso,
+            15, 
             "E como diria Baco\n" + 
             "\n\"Quem é ela? Quem é ela?\"\n" +
             "Reflexivo acendi um cigarro\n" +
             "Até que ouvi gritarem um nome\n" +
             "Estela\n",
-            Narciso,
-            15, 
             null, 
             null, 
             null,
@@ -83,12 +83,12 @@ public class App {
 
         else if (escolha == 2){ //.equals é uma maneira de comparar string já que esse tipo não é primitivo não podendo assim usar ==
             capB2.mostrar();
-            escolha2 = capB2.escolher(); 
+            escolha = capB2.escolher(); 
 
-            if (escolha2 == 1){        
+            if (escolha == 1){        
                 capC1.mostrar();
             }
-            else if(escolha2 == 2){
+            else if(escolha == 2){
                 capC2.mostrar();
             }
             }
--- O Conto de Estela e Naciso/src/O Conto/src/capitulo/src/capitulo.java
+++ O Conto de Estela e Naciso/src/O Conto/src/capitulo/src/capitulo.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class capitulo {
    String título; //atributos do capítulo
-   String texto;
    Personagem personagem;
    int alteracaoCoragem;
+   String texto;
    String pergunta;
    String escolha1;
    String escolha2;
    Scanner escaneador;
 
-   capitulo (String título, String texto, Personagem personagem, int alteracaoCoragem, 
-   String pergunta, String escolha1, String escolha2, Scanner escaneador){ //construtor com parâmetros
+   capitulo (String título, Personagem personagem, int alteracaoCoragem, 
+   String texto, String pergunta, String escolha1, String escolha2, Scanner escaneador){ //construtor com parâmetros
 
     this.título = título;
-    this.texto = texto;
     this.personagem = personagem;
     this.alteracaoCoragem = alteracaoCoragem;
+    this.texto = texto;
     this.pergunta = pergunta;
     this.escolha1 = escolha1;
     this.escolha2 = escolha2;
@@ -28,35 +28,31 @@ public class capitulo {
         System.out.println("\n______________________________________\n");
         System.out.println(this.título);
         System.out.println("______________________________________\n\n");
-        System.out.println(this.texto);
         this.personagem.alterarCoragem(this.alteracaoCoragem);
-        if(this.pergunta != null){
-        System.out.println(this.pergunta);
-        }
-        if (this.escolha1 != null){
-        System.out.println("-" + this.escolha1);
-        }
-        if (this.escolha2 != null){
-        System.out.println("-" + this.escolha2);
-        }      
+        System.out.println(this.texto);     
     }
 
     int escolher (){
         int escolha = -1; //um valor para representar um caso de erro
+        
+        if(this.pergunta != null){
+           while (escolha == -1){ //significa que se nenhuma escolha válida foi selecionada continua pedindo para seleconar uma escolha
+            System.out.println(this.pergunta);
+            if (escolha1 != null && escolha2 != null){
+                System.out.println("-" + this.escolha1);
+                System.out.println("-" + this.escolha2);
+                String escolhaDigitada = escaneador.nextLine(); //capturar o que o usuário escolheu/escreveu
+                System.out.println();
+                
+                if (escolhaDigitada.equals(escolha1)){
+                    escolha = 1;
+                }
 
-        if (escolha1 != null && escolha2 != null){
-            Scanner escaneador = new Scanner(System.in); //capturar a entrada do usuário com a variável do tipo Scanner que se chama escaneador
-            String escolhaDigitada = escaneador.nextLine(); //capturar o que o usuário escolheu/escreveu
-
-            if (escolhaDigitada.equals(escolha1)){
-                escolha = 1;
-            }
-
-            else if (escolhaDigitada.equals(escolha2)){
-                escolha = 2;
-            }
-        escaneador.close(); //fechando o escaneador
-        }
+                else if (escolhaDigitada.equals(escolha2)){
+                    escolha = 2;
+                }
+                }
+            }}
         return escolha;
     }
 }
